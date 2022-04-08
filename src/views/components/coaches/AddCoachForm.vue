12bd02562e6e7351bcae90cff1d2db4194683dc4
unknown <azharelamyr@gmail.com>
2022-04-08 16:40:20 +0200

    validation over coaches and sweet alerts

--- src/views/components/coaches/AddCoachForm.vue
+++ src/views/components/coaches/AddCoachForm.vue
@@ -2,16 +2,15 @@
   <div class="container-fluid mt-5">
     <div class="row justify-content-center">
       <div class="col-10 col-md-8 col-lg-6">
-        <input
-          v-model="name"
-          name="name"
-          placeholder="name"
-          type="text"
-          class="my-2 form-control"
-        />
-        <div class="text-center mt-5">
-          <button class="btn btn-success" @click="addCoach()">Add coach</button>
-        </div>
+        <Form @submit="addCoach">
+          <Field
+            v-model="name" name="name" placeholder="name" type="text" class="my-2 form-control" rules="required"
+          />
+          <ErrorMessage class="text-danger small" name="name" /><br />
+          <div class="text-center mt-5">
+            <input class="btn btn-success" type="submit" value="Add coach" />
+          </div>
+        </Form>
       </div>
     </div>
   </div>
@@ -19,8 +18,15 @@
 
 <script>
 import CoachService from "../../../services/CoachService";
+import { Form, Field, ErrorMessage } from "vee-validate";
+import Swal from "sweetalert2";
 export default {
   name: "add-coach",
+  components: {
+    Form,
+    Field,
+    ErrorMessage,
+  },
   data() {
     return {
       name: "",
@@ -28,15 +34,22 @@ export default {
   },
   methods: {
     addCoach() {
-      if (this.name) {
+      // if (this.name) {
         let Data = {
           name: this.name,
         };
         CoachService.create(Data).then((res) => {
+          Swal.fire(
+                  'Added!',
+                  'A coach has been added.',
+                  'success'
+                ).then(()=>{
+                 this.$router.push("/coaches");
+                })
           console.log(res);
-          this.$router.push("/coaches");
+         
         });
-      }
+      // }
     },
   },
 };
--- src/views/components/coaches/EditCoachForm.vue
+++ src/views/components/coaches/EditCoachForm.vue
@@ -2,18 +2,20 @@
   <div class="container-fluid mt-5">
     <div class="row justify-content-center">
       <div class="col-10 col-md-8 col-lg-6">
-        <input
+         <Form @submit="editCoach($route.params.id)">
+        <Field
           v-model="name"
           name="name"
           placeholder="name"
           type="text"
           class="my-2 form-control"
         />
+          <ErrorMessage class="text-danger small" name="name" /><br/>
         <div class="text-center mt-5">
-          <button class="btn btn-success" @click="editCoach($route.params.id)">
-            Edit coach
-          </button>
+          <input class="btn btn-success" type="submit" value="Edit Coach"/>
+        
         </div>
+       </Form>
       </div>
     </div>
   </div>
@@ -21,7 +23,14 @@
 
 <script>
 import CoachService from "../../../services/CoachService";
+import { Form, Field, ErrorMessage } from "vee-validate";
+import Swal from "sweetalert2";
 export default {
+    components: {
+    Form,
+    Field,
+    ErrorMessage,
+  },
   data() {
     return {
       name: "",
@@ -35,15 +44,21 @@ export default {
     },
 
     editCoach(id) {
-      if (this.name) {
+   
         let Data = {
           name: this.name,
         };
         CoachService.update(Data, id).then((res) => {
           console.log(res);
-          this.$router.push("/coaches");
+             Swal.fire(
+                  'Edited!',
+                  'A coach has been Edited.',
+                  'success'
+                ).then(()=>{
+                 this.$router.push("/coaches");
+                })
         });
-      }
+    
     },
   },
   async created() {
